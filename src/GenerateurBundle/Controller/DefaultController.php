aac622044b84796e70709a6c8f8c12529a903b60
Arthur Moll <arth.moll@gmail.com>
2016-02-17 13:08:47 +0100

    ajout d une page d accueil avec controle pour les utilisateurs

--- src/GenerateurBundle/Controller/DefaultController.php
+++ src/GenerateurBundle/Controller/DefaultController.php
@@ -6,5 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    public function indexAction()
+    {
+        $pages=$this->getDoctrine()->getRepository('GenerateurBundle:Page')->findAll();
+
+        return $this->render('GenerateurBundle:Default:index.html.twig', array(
+            'pages'=>$pages
+        ));
+    }
 
 }
